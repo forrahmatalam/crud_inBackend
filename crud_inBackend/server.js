2b8d800a6aa7433f7b25a7ec74686cc8c605fc96
Rahmat Alam <forrahmatalam@users.noreply.github.com>
2026-09-08 11:05:06 +0530

    crud operations

--- crud_inBackend/server.js
+++ crud_inBackend/server.js
@@ -3,27 +3,42 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-// middleware
+// middleware for accepting or parse json data 
 app.use(express.json());
 
-let users = [{
-    name: "motorola",
-    age: 20,
-}];
+let users = [];
 
-let body;
+
 
 // Create
 app.post("/create", (req, res) => {
-    body = req.body;
-    res.send(body);
+    let body = req.body;
+    users.push(body);
+    res.send("User created");
 });
+
+//delete
+app.delete("/delete/:id",(req,res)=>{
+     let {id} =req.params; //req.params parameter ki value leta hai mean id ki 
+     let userData =users.filter((val)=>val.id!==id);
+     users=userData; //user data ko user me update kr rhe hai 
+     res.send("User deleted");
+});
+
+//update
+app.put("/update/:id",(req,res)=>{
+    let {id} =req.params;
+    let {name} =req.body;
+    let updatedUser =users.map((val)=>val.id==id ?{...val,name}:val);
+    users=updatedUser;
+    res.send("updated user sucessfully");
+})
 
 // Get - Read
 app.get("/", (req, res) => {
-    res.send(body);
+    res.send(users);
 });
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
-});
+}); 
